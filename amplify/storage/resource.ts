21756136566a1b6b3acc9093a0130908262ef5e3
ialmosara <ialmosara@blockgraph.co>
2025-05-27 14:27:02 -0400

    add storage backend

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,5 +1,11 @@
 import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
-    name: 'bg-audience-data-3008 '
+    name: 'bg-audience-data-3008',
+    access: (allow) => ({
+        'cleanroom/data/table/*': [
+            allow.guest.to(['read']),
+            allow.entity('identity').to(['read', 'write', 'delete'])
+        ],
+    })
 });
